ae02cd692e2690d2c26bc18a9820680e1b971fa1
Sriram99 <132443797+112201019@users.noreply.github.com>
2024-07-06 17:48:14 +0530

    Add files via upload
    A simple calculator done with Basic HTML, CSS and Javascript.

---
+++ calc.js
@@ -0,0 +1,19 @@
+let calculation=localStorage.getItem('calculation') || '';
+const varr=document.querySelector('.paraa');
+varr.innerHTML=calculation;
+function func(para){
+    if(para==='='){
+        calculation=eval(calculation);
+        localStorage.setItem('calculation', calculation);
+    }
+    else if(para==='clear'){
+        calculation='';
+        localStorage.setItem('calculation', calculation);
+    }
+    else{
+        calculation+=para;
+    }
+    const varr=document.querySelector('.paraa');
+    varr.innerHTML=calculation;
+    // console.log(calculation);
+}
